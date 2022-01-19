f7de25e589e2921a8694ceae37f070efd14ebf50
dbha <dbha0719@gmail.com>
2022-01-19 18:20:23 +0900

    update

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot Pipeline test + Tanzu TAP!";
+		return "Greetings from Spring Boot Pipeline TEST + Tanzu TAP!";
 	}
 
 }
