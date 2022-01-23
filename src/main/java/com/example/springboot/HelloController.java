2f0a054cb25e6d7e82251f6eac9804d29a491221
dbha <dbha0719@gmail.com>
2022-01-23 21:46:25 +0900

    update

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Spring Boot Pipeline Test + Tanzu TAP";
+		return "Spring Boot Live update Test + Tanzu TAP";
 	}
 
 }
