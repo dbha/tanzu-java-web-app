5da0b5e47a2a7ee39b5d8c09a26ee5fb75e5bfc8
dbha <dbha0719@gmail.com>
2022-04-27 12:25:33 +0900

    update

--- src/test/java/com/example/springboot/HelloControllerTest.java
+++ src/test/java/com/example/springboot/HelloControllerTest.java
@@ -21,11 +21,11 @@ class HelloControllerTest {
 
     @Test
     void index() throws Exception {
-        assertEquals("Spring Boot Live update Test + Tanzu TAP", controller.index());
+        assertEquals("Spring Boot For Tanzu TAP", controller.index());
 
         mockMvc
             .perform(get("/"))
             .andExpect(status().isOk())
-            .andExpect(content().string("Spring Boot Live update Test + Tanzu TAP"));
+            .andExpect(content().string("Spring Boot For Tanzu TAP"));
     }
 }
